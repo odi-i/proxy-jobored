f0e6c05569683cff9d2417fedfcc0da327466536
Oleg Tokarev <69398712+odi-i@users.noreply.github.com>
2023-07-02 23:03:39 +0300

    fix: remove /

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const cookiePerIP = new LRUCache({
     updateAgeOnGet: false,
     updateAgeOnHas: false,
 });
-const allowList = ['http://localhost:5173/','https://papaya-entremet-3046cd.netlify.app']
+const allowList = ['http://localhost:5173','https://papaya-entremet-3046cd.netlify.app']
 
 const corsOptions = {
     origin:(origin:any, callback:any)=> {
